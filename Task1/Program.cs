e606c4d9302fdbb724a6f0e401d8e8022bca1a4c
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-10 02:38:25 +0700

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая на вход принимает трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+int SecondNumber(int x)
+    {
+        int a = x / 100;
+        int b = x / 10 - a * 10;
+        int c = x - b * 10 - a * 100;   
+        return b;
+    }   
+
+Console.Write("Please, enter the three-digit number: ");
+
+int TDNumber = int.Parse(Console.ReadLine());
+
+int FindNumber = SecondNumber(TDNumber);
+
+Console.Write("The second number of three-digital number is: ");
+Console.Write(FindNumber);
