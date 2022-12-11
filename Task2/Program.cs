9028194ca200b1eb30808f22cee36cf720363789
Vitaly <Vitalion.Bersenev@gmail.com>
2022-12-11 19:29:16 +0700

    task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Please, enter the number: ");
+
+int Number = int.Parse(Console.ReadLine());
+
+string NumberToText = Convert.ToString(Number);
+if (NumberToText.Length > 2)
+    {
+    Console.WriteLine("The third number is : " + NumberToText[2]);
+    }
+else
+    {
+    Console.WriteLine("Don't find the third number");
+    }
